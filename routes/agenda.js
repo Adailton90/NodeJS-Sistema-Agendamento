7d62d36af0de86c40ef8dc01a69a59dd227b3910
Adailton Silva <adailton.silva10@live.com>
2019-08-28 17:00:37 -0300

    metodos de busca

--- routes/agenda.js
+++ routes/agenda.js
@@ -1,48 +1,139 @@
 const agendaRoutes = (app, fs) => {
     const dataService = require('./class');
-    //const agendaPath = './data/agenda.json';
 
-    //lISTAGEM
-    app.get('/sistemagendamento', async(req, res) => {
-        const data = await dataService.redyAgenda();
-        return res.send(data);
+    //lIST-ALL
+    app.get('/sistemaDeAgendamento', async(req, res) => {
+        try {
+            const data = await dataService.redyAgenda();
+            return res.send(data);
+
+        } catch (error) {
+            return res.status(400).send({error: 'Erro nos reistros da Agenda'});
+        }
+        
     });
 
+
+    //LIST-BY-ID
+    app.get('/sistemaDeAgendamento/:id', async(req, res) => {
+        try {
+            const busca = req.params.id;
+            const data = await dataService.redyAgenda();
+            var test = 0;
+            data.forEach(function(elemento){
+                var intervalo = elemento.intervals;
+                intervalo.forEach(function(index){
+                    if(index.id == busca){     
+                        test = 1;
+                        res.send(index);         
+                    }                 
+                });                                   
+                 
+            });
+            if(test != 1){
+                res.send('ID informado nao é validao');
+            }
+        } catch (error) {
+            return res.status(400).send({error: 'Erro ao tentar localizar horario na agenda'});
+        }
+    });
+
+    //LIST-BY-DAY
+    app.get('/listByDay/:day', async(req, res) => {
+        try {
+            const busca = req.params.day;
+            const data = await dataService.redyAgenda();
+            var test = 0;
+
+            data.forEach(function(elemento){
+                if(elemento.day == busca){
+                    test = 1; 
+                    var horarios =  elemento.intervals; 
+                    var response = {status: 'Horários disponíveis para esta data', horarios};                 
+                    res.send(response);
+                }                        
+                
+            });   
+            if(test != 1){
+                res.send('Todos horários estão livre para esta data, deseja agendar?');
+            }            
+        } catch (error) {
+            return res.status(400).send({error: 'Erro ao tentar localizar evento na agenda'});
+        }
+    });
+
+    app.get('/listAllByHours/:start&end', async(req, res) => {
+        try {
+            const busca1 = req.params.start;
+            //const busca2 = req.params.end;
+            const data = await dataService.redyAgenda();
+            var test;
+            console.log(busca1);
+            /*
+            data.forEach(function(elemento){
+                var intervalo = elemento.intervals;
+                intervalo.forEach(function(index){
+                    if(index.start == busca.start &&  index.end == busca.end){     
+                        test = 1;
+                        console.log(index);         
+                    }                 
+                });                 
+                
+            });  
+            res.send(busca1);
+            if(test != 1){
+                res.send('Todos horários estão livre para esta data, deseja agendar?');
+            }    */        
+        } catch (error) {
+            return res.status(400).send({error: 'Erro ao tentar localizar evento na agenda'});
+        }
+    });
+
+     //DELETE
+     app.delete('/sistemaDeAgendamento/:id', async(req, res) => {
+        try {
+            const busca = req.params.id;
+            const data = await dataService.redyAgenda();
+            
+           // 
+           data.forEach(function(elemento){
+               var intervalo = elemento.intervals;
+               intervalo.forEach(function(index){
+                   if(index.id === busca){     
+                        res.send('evento deletado');                   
+                   };                    
+               });                                      
+                
+            });   
+              
+
+        } catch (error) {
+            return res.status(400).send({error: 'Erro ao tentar cadastrar'});
+        }
+        
+        
+    });
+
+    
 
     //CADASTRO
-    app.post('/sistemagendamento', async(req, res) => {
-        const { body: data } = req;
-        const agendaAtual = await dataService.redyAgenda();
+    app.post('/sistemaDeAgendamento', async(req, res) => {
+        try {
+            const { body: data } = req;
+            const agendaAtual = await dataService.redyAgenda();
+            agendaAtual.push(data);
 
-        agendaAtual.push(data);
-        const retorno = await dataService.saveAgenda(agendaAtual);
-        return res.send(retorno);
+            const retorno = await dataService.saveAgenda(agendaAtual);
+            //var response = {status:'horário inserido na agenda!', resultado: retorno}
+            return res.send('horário inserido na agenda!');
+
+        } catch (error) {
+            return res.status(400).send({error: 'Erro ao tentar cadastrar'});
+        }
+        
+        
     });
 
-
-
-
-
-    //DELETE
-    app.delete('/sistemagendamento/', (req, res) => {
-        fs.readFile(agendaPath, 'utf8', (err, data) => {
-            if (err) {
-                console.log("Erro ao ler arquivo");
-            } else {
-                var obj = JSON.parse(data);
-                delete obj.agenda[(req.body.id - 1)];
-
-                fs.writeFile(agendaPath, JSON.stringify(obj), function(err) {
-                    if (err) {
-                        console.log("Erro ao tentar salvar arquivo");
-                    } else {
-                        var response = { status: 'Sucesso', resultado: 'Excluido com sucesso' };
-                        res.json(response);
-                    }
-                });
-            }
-        });
-    });
 };
 
 
--- server.js
+++ server.js
@@ -5,19 +5,8 @@ const cors = require('cors');
 
 const app = express();
 
-
-
 app.use(bodyParser.urlencoded({ extended:true }));
 app.use(bodyParser.json());
-app.use(cors());
-/*app.use(function(req, res, next){
- res.setHeader("Access-Control-Allow-Origin", "*");
- res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
- res.setHeader("Access-Control-Allow-Headers", "content-type");
- res.setHeader("Content-Type", "application/json");
- res.setHeader("Access-Control-Allow-Credentials", true);
- next();
-});*/
 
 const routes = require('./routes/routes.js')(app, fs);
 const server = app.listen(8090, function(){
